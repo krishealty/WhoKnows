002fe8a5397b83e80764a05be7674f4bc766567f
Krish Lalwani <krishlalwani846@gmail.com>
2024-06-12 14:48:08 +0530

    Add files via upload

---
+++ test/plugins/deprecated-plugin-format.rb
@@ -0,0 +1,9 @@
+# Following document is a file of "WhoKnows" by Krish Lalwani, whose
+# redistribution and commercial usage is prohibited. Please visit
+# the official web site for Licensing information.
+
+Plugin.define "Deprecated-Plugin-Format" do
+author "Krish Lalwani"
+description "Tests the deprecated plugin format for WhoKnows."
+
+end
